c74a54088d24a2b8afa539a55eabb897d0edb45c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-23 16:25:26 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,12 @@
 class SessionsController < ApplicationController
   def create
-     @user = User.find_or_create_by(uid: auth['uid']) do |u|
-       u.name = auth['info']['name']
-       u.email = auth['info']['email']
-       u.image = auth['info']['image']
-     end
+      user = User.find_or_create_by(:uid => auth['uid']) do |user|
+        user.name = auth['info']['name']
+      end
+      session[:user_id] = user.try(:id)
+    end
 
-     session[:user_id] = @user.id
-
-     render 'welcome/home'
-   end
-
-   private
-
-   def auth
-     request.env['omniauth.auth']
-   end
+    def auth
+      request.env['omniauth.auth']
+    end
 end
